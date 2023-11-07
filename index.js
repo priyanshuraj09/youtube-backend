3806b636604f065c9e06fbbc4feb14acbd52858b
priyanshuraj09 <priyanshuverma6891@gmail.com>
2023-11-07 21:39:07 +0530

    update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const authRouter = require("./routes/authRoutes");
 
 mongoose
   .connect(
-    "mongodb+srv://youtubeclone:1234@cluster0.5ofd4si.mongodb.net/youtubeclone_220240101004"
+    "mongodb+srv://youtubeclone:1234@cluster0.5ofd4si.mongodb.net/youtubeclone_220240101109"
   )
   .then(() => {
     console.log("DB connected");
